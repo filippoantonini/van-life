e063782fd36e0b767a4d5235f6259863ad3c02b0
filippoantonini <93533211+filippoantonini@users.noreply.github.com>
2023-12-09 18:21:19 +0100

    adding vans section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
+import Vans from "./pages/Vans.jsx"
+
 
 function App() {
 
@@ -11,10 +13,14 @@ function App() {
         <nav>
           <Link to="/about">About</Link>
         </nav>
+        <nav>
+          <Link to="/Vans">Vans</Link>
+        </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/About" element={<About />}/>
+        <Route path="/Vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,9 @@
+// import { Link } from "react-router-dom"
+
+export default function Vans(){
+    return(
+        <div>
+            <h1>Vans page goes here 🚐</h1>
+        </div>
+    )
+}
