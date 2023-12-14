4f94c3accf7b536f89ee968157bff96b085c0a03
filippoantonini <93533211+filippoantonini@users.noreply.github.com>
2023-12-14 22:15:36 +0100

    nesting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
 import VanDetail from "./pages/VanDetail.jsx"
+import Layout from "./components/Layout.jsx"
 
 function App() {
 
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VANLIFE</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-        <nav>
-          <Link to="/Vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/About" element={<About />}/>
-        <Route path="/Vans" element={<Vans />} />
-        <Route path="/Vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/About" element={<About />}/>
+          <Route path="/Vans" element={<Vans />} />
+          <Route path="/Vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -30,4 +30,4 @@ export default function VanDetail() {
         </div>
         </div>
     );
-    }
+}
