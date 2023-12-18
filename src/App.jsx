bb62ced123f6afcc5c88473da22e2cc345954f63
filippoantonini <93533211+filippoantonini@users.noreply.github.com>
2023-12-18 22:56:54 +0100

    nesting

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Income from "./pages/Host/Income.jsx"
 import Reviews from "./pages/Host/Reviews.jsx"
 import HostVans from "./pages/Host/HostVans"
 import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import Layout from "./components/Layout.jsx"
 import HostLayout from "./components/HostLayout.jsx"
 
@@ -28,7 +31,11 @@ function App() {
               <Route path="income" element={<Income />} />
               <Route path="reviews" element={<Reviews />} />
               <Route path="vans" element={<HostVans />} />
-              <Route path="vans/:id" element={<HostVanDetail />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
 
         </Route>
--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, Outlet } from "react-router-dom"
 
 const vanElements = [ {id: "1", name: "Modest Explorer", price: 60, description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", type: "simple" },
 { id: "2", name: "Beach Bum", price: 80, description: "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/beach-bum.png", type: "rugged" },
@@ -38,6 +38,7 @@ export default function HostVanDetail() {
                         <h4>${van.price}/day</h4>
                     </div>
                 </div>
+                <Outlet />
             </div>
         </section>
     );
